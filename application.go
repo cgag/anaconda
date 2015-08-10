3654b091b3dabf2edfd16a4dc61589755abfed23
Curtis Gagliardi <curtis@curtis.io>
2015-08-09 21:49:36 -0700

    Add more Resources to parse.

--- application.go
+++ application.go
@@ -22,10 +22,17 @@ type EndpointStatus struct {
 }
 
 type Resources struct {
-	Users    Resource `json:"users"`
-	Statuses Resource `json:"statuses"`
-	Help     Resource `json:"help"`
-	Search   Resource `json:"search"`
+	Users          Resource `json:"users"`
+	Friendships    Resource `json:"friendships"`
+	Followers      Resource `json:"followers"`
+	Account        Resource `json:"account"`
+	Media          Resource `json:"media"`
+	DirectMessages Resource `json:"direct_messages"`
+	Mutes          Resource `json:"mutes"`
+	Favorites      Resource `json:"favorites"`
+	Statuses       Resource `json:"statuses"`
+	Help           Resource `json:"help"`
+	Search         Resource `json:"search"`
 }
 
 type Resource map[Endpoint]EndpointStatus
@@ -81,21 +88,38 @@ type Endpoint string
 type ResourceRaw map[Endpoint]EndpointStatusRaw
 
 type ResourcesRaw struct {
-	Users    ResourceRaw `json:"users"`
-	Statuses ResourceRaw `json:"statuses"`
-	Help     ResourceRaw `json:"help"`
-	Search   ResourceRaw `json:"search"`
+	Users          ResourceRaw `json:"users"`
+	Friendships    ResourceRaw `json:"friendships"`
+	Followers      ResourceRaw `json:"followers"`
+	Account        ResourceRaw `json:"account"`
+	DirectMessages ResourceRaw `json:"direct_messages"`
+	Mutes          ResourceRaw `json:"mutes"`
+	Favorites      ResourceRaw `json:"favorites"`
+	Statuses       ResourceRaw `json:"statuses"`
+	Help           ResourceRaw `json:"help"`
+	Search         ResourceRaw `json:"search"`
 }
 
 func parseTimes(raw RateLimitStatusRawTimes) RateLimitStatus {
 	var parsedStatus RateLimitStatus
 
-	parsedStatus.RateLimitContext = RateLimitContext{raw.RateLimitContext.AccessToken}
+	rr := raw.Resources
+
+	parsedStatus.RateLimitContext = RateLimitContext{
+		raw.RateLimitContext.AccessToken,
+	}
+
 	parsedStatus.Resources = Resources{
-		Help:     parseRawResource(raw.Resources.Help),
-		Search:   parseRawResource(raw.Resources.Search),
-		Statuses: parseRawResource(raw.Resources.Statuses),
-		Users:    parseRawResource(raw.Resources.Users),
+		Users:          parseRawResource(rr.Users),
+		Friendships:    parseRawResource(rr.Friendships),
+		Followers:      parseRawResource(rr.Followers),
+		Account:        parseRawResource(rr.Account),
+		DirectMessages: parseRawResource(rr.DirectMessages),
+		Mutes:          parseRawResource(rr.Mutes),
+		Favorites:      parseRawResource(rr.Favorites),
+		Statuses:       parseRawResource(rr.Statuses),
+		Help:           parseRawResource(rr.Help),
+		Search:         parseRawResource(rr.Search),
 	}
 
 	return parsedStatus
